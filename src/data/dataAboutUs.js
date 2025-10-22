b6479e2b94f94f3fa7c9e5cde5d00811535b93eb
Fiamma B <brizuelafiamma6@gmail.com>
2025-10-22 00:14:16 -0300

    fix: componente para About Us + nuevos estilos + refactorizacion de DataAboutUs

--- src/data/dataAboutUs.js
+++ src/data/dataAboutUs.js
@@ -1,17 +1,16 @@
-const dataAbout = [
-    {paragraph: 'Somos una empresa de gestión inmobiliaria de Murcia, situados en pleno centro de nuestra capital desde 1998, lo que nos avala por nuestra experiencia al tener un profundo conocimiento en este sector como expertos en la zona.'},
+const dataAbout = {
+    
+    intro:['Somos una empresa de gestión inmobiliaria de Murcia, situados en pleno centro de nuestra capital desde 1998, lo que nos avala por nuestra experiencia al tener un profundo conocimiento en este sector como expertos en la zona.',
+        'Ofrecemos una atención cualificada, personalizada y profesional, dirigida a asegurar en garantía del cliente con nuestros servicios, una operación transparente, rápida y profesional, comprometiéndonos a que reciban la información necesaria para llevarla a buen fin.'
 
-    {paragraph: 'Ofrecemos una atención cualificada, personalizada y profesional, dirigida a asegurar en garantía del cliente con nuestros servicios, una operación transparente, rápida y profesional, comprometiéndonos a que reciban la información necesaria para llevarla a buen fin.'},
-
-    {paragraph: 'La compra-venta o alquiler de una vivienda es un paso muy importante, por este motivo ofrecemos en nuestros servicios, confianza, compromiso y experiencia dando un trato personalizado.'},
-
-    {paragraph: 'Garantizamos nuestra actividad profesional mediante un seguro de responsabilidad civil de 1.500.000€.'},
-
-    {paragraph: 'Realizamos valoraciones reales con información veraz y actualizada de tu inmueble, no damos los precios aspiracionales que figuran en los portales, ni los dudosos precios que reflejan las estadísticas oficiales, ni infravaloramos los precios para así vender o alquilar más deprisa, todo ello, basándonos en nuestra extensa experiencia en el sector y utilizando modernos y avanzados sistemas de valoración avalados siempre por el Colegio de Registradores de España, el Ilustre Colegio Notarial de Murcia.'},
-
-    {paragraph: 'Utilizamos los mejores y más diversos medios de publicidad, estando presentes en todas las zonas de nuestra ciudad en las primeras posiciones de los más importantes y mejores portales inmobiliarios a nivel NACIONAL, siempre en la primera pagina de la sección a la que corresponde su inmueble y con fotos PREMIUM.'},
-
-    {paragraph: 'Cientos de clientes satisfechos hacen honor a nuestro código deontológico de conducta profesional, el cual hemos seguido, seguimos y seguiremos siempre cumpliendo con nuestro lema.'}
-]
+    ],
+    gridItems: ['Ofrecemos una atención cualificada, personalizada y profesional, dirigida a asegurar en garantía del cliente con nuestros servicios, una operación transparente, rápida y profesional, comprometiéndonos a que reciban la información necesaria para llevarla a buen fin.',
+     'Garantizamos nuestra actividad profesional mediante un seguro de responsabilidad civil de 1.500.000€.',
+     'Utilizamos los mejores y más diversos medios de publicidad, estando presentes en todas las zonas de nuestra ciudad en las primeras posiciones de los más importantes portales inmobiliarios a nivel NACIONAL, siempre en la primera pagina de la sección a la que corresponde su inmueble y con fotos PREMIUM.',
+     'Realizamos valoraciones reales con información veraz y actualizada de tu inmueble, basándonos en nuestra extensa experiencia en el sector y utilizando modernos y avanzados sistemas de valoración avalados por el Colegio de Registradores de España, el Ilustre Colegio Notarial de Murcia.'
+    
+    ],
+     conclution:['Cientos de clientes satisfechos hacen honor a nuestro código deontológico de conducta profesional, el cual hemos seguido, seguimos y seguiremos siempre cumpliendo con nuestro lema.'  ]
+};
 
 export default dataAbout;
---
+++ src/styles/AboutComponentStyle.js
@@ -0,0 +1,8 @@
+const AboutUsStyles = {
+    container:'container mx-auto py-16 px-4',
+    title: 'italic text-center py-4 font-medium text-blue-900 text-3xl ',
+    paragraph: 'text-base text-center p-5 md:max-w-5xl mx-auto text-gray-700',
+    gridItem: 'text-base  text-start p-5 md:max-w-5xl mx-auto text-gray-700   ',
+    gridCols:'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4',
+};
+export default AboutUsStyles;
