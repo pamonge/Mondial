5c1f014e189ce7e632ecb20676c51c3bd6c1de66
Fiamma B <brizuelafiamma6@gmail.com>
2025-10-23 21:23:05 -0300

    feat: lista de  servicios (comprar,vender,alquilar)

---
+++ src/components/services/CardServiceComponent.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import servicesStyle from "../../styles/servicesComponentStyle"
+import { Link } from "react-router-dom";
+
+// Tarjeta individual de servicio para la lista de servicios
+export const CardServiceComponent = ({ info, onClick }) => {
+    return (
+        //link a la página correspondiente al servicio
+        <Link to={info.linkData} className={servicesStyle.cardContainer} onClick={onClick}>
+            <div className={servicesStyle.imgContainer}>
+                <img
+                    src={`src/assets/img/${info.img}`}
+                    alt={info.title}
+                    className={servicesStyle.imgCard}
+                />
+            </div>
+            <div className={servicesStyle.textContainer}>
+                <h3 className={servicesStyle.h3Styles}>
+                    Quiero <strong>{info.service}</strong>
+                    <br />
+                    una propiedad
+                </h3>
+
+            </div>
+        </Link>
+    );
+}
+
---
+++ src/components/services/ListCardComponent.jsx
@@ -0,0 +1,33 @@
+
+
+import React from "react";
+import dataCarousel from "../../data/dataCarousel";
+
+import { CardServiceComponent } from "./CardServiceComponent";
+
+import servicesStyle from "../../styles/servicesComponentStyle";
+
+export const ListCardComponent = () => {
+    return (
+
+        <div className={servicesStyle.sectionContainer}>
+
+            {/*  Título principal  */}
+            <h2 className={` ${servicesStyle.mainTitleColor} ${servicesStyle.mainTitle}`}>
+                Somos especialistas en el mercado
+                <br />
+                residencial en Murcia
+            </h2>
+
+
+            <div className={servicesStyle.gridContainer}>
+                {
+                    dataCarousel.map((service) => (
+                        //  Se Renderiza la tarjeta 
+                        <CardServiceComponent key={service.id} info={service} />
+                    ))
+                }
+            </div>
+        </div>
+    );
+}
