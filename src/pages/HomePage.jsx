caf17e7da0ac7ff7b50af2dd8bde1ca17e3ba96f
Fiamma B <brizuelafiamma6@gmail.com>
2025-10-23 21:25:40 -0300

    refactor: agrege componente para visualizar servicios

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { HeroComponent } from '../components/header/HeroComponent'
 import { MondialSlogan } from '../components/header/MondialSlogan'
 import { CardHomeComponent } from '../components/home/CardHomeComponent'
-
+//es la lista de servicios que se muestra en la home
+import { ListCardComponent } from '../components/services/ListCardComponent'
 
 export const HomePage = () => {
   return (
     <div>
       <HeroComponent />
-      
-      
+      <ListCardComponent />
+
       <MondialSlogan />
     </div>
   )
