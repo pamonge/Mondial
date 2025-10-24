e0c19bd08fa8eb1a8a3011f9878257a4c14a1e41
Fiamma B <brizuelafiamma6@gmail.com>
2025-10-23 21:18:42 -0300

    refactor: agrege service al objeto q describe si se compra,vende o alquila

--- src/data/dataCarousel.js
+++ src/data/dataCarousel.js
@@ -2,6 +2,8 @@ const dataCarousel = [
     {
         id : 1,
         title: 'Quiero comprar un propiedad',
+        service:'comprar',
+        img:"comprar.jpg",
         description: 'Encuentra el hogar que siempre soñaste. En Mondial inmobiliaria te acompañamos en cada paso para que adquieras la propiedad ideal: segura, bien ubicada y ajustada a tu presupuesto. Ya sea tu primera vivienda, un departamento moderno en el centro o una casa espaciosa en un barrio tranquilo, tenemos las mejores opciones en la región. ¡Tu futuro comienza aquí!',
         category: 'Compra',
         linkData: '/buy'
@@ -9,6 +11,8 @@ const dataCarousel = [
     {
         id : 2,
         title: 'Quiero alquilar una propiedad',
+        service:'alquilar',
+        img:"comprar.jpg",
         description: 'Viví donde siempre quisiste, sin esperar a comprar. Ofrecemos una selección cuidadosa de propiedades en alquiler, en las zonas más convenientes de la ciudad. Con procesos ágiles, contratos transparentes y asesoría personalizada, te ayudamos a encontrar el espacio perfecto para tu estilo de vida o tu negocio. ¡Listo para mudarte hoy!',
         category: 'Alquiler',
         linkData: '/rent'
@@ -16,6 +20,8 @@ const dataCarousel = [
     {
         id : 3,
         title: 'Quiero vender mi propiedad',
+        service:'vender',
+        img:"sell-page.jpg",
         description: 'En Mondial Inmobiliaria, sabemos que una vivienda es solo estructura, pero un hogar es donde late el corazón. Con 25 años de experiencia en el sector inmobiliario, colocamos tu hogar y tus intereses  en el epicentro de todo lo que hacemos. No vendemos casas, creamos oportunidades, construimos confianza y entregamos resultados reales. Gracias a nuestra exclusiva red de compradores cualificados, estrategias de marketing de alto impacto y un enfoque 100% personalizado, transformamos la venta de tu propiedad en una experiencia fluida, transparente y, sobre todo, exitosa. Porque en Mondial Inmobiliaria, lo que importa para ti... nos importa de verdad.',
         category: 'Venta',
         linkData: '/sell'
