e8c955a3e05d36849ff720847a5e4de894208bca
Fiamma B <brizuelafiamma6@gmail.com>
2025-11-03 22:12:32 -0300

    feat: agrege imagenes como nuevo item en un array de arrays

--- src/data/dataAboutUs.js
+++ src/data/dataAboutUs.js
@@ -1,12 +1,13 @@
+
 const dataAbout = {
     image:"AboutUs.png",
     intro:['Somos una empresa de gestión inmobiliaria de Murcia, situados en pleno centro de nuestra capital desde 1998, lo que nos avala por nuestra experiencia al tener un profundo conocimiento en este sector como expertos en la zona.'
 
     ],
-    gridItems: ['Ofrecemos en nuestros servicios, confianza, compromiso y experiencia dando un trato personalizado.',
-     'Garantizamos nuestra actividad profesional mediante un seguro de responsabilidad civil de 1.500.000€.',
-     'Utilizamos los mejores y más diversos medios de publicidad, estando presentes en todas las zonas de nuestra ciudad en las primeras posiciones de los más importantes portales inmobiliarios a nivel NACIONAL, siempre en la primera pagina de la sección a la que corresponde su inmueble y con fotos PREMIUM.',
-     'Realizamos valoraciones reales con información veraz y actualizada de tu inmueble, basándonos en nuestra extensa experiencia en el sector y utilizando modernos y avanzados sistemas de valoración avalados por el Colegio de Registradores de España, el Ilustre Colegio Notarial de Murcia.'
+    gridItems:[ [`Trust.png`,'Ofrecemos en nuestros servicios, confianza, compromiso y experiencia dando un trato personalizado.'],
+     ["Protect.png",'Garantizamos nuestra actividad profesional mediante un seguro de responsabilidad civil de 1.500.000€.'],
+     ["Camera.png",'Destacamos su propiedad con máxima visibilidad nacional y fotografías premium profesionales.'],
+     ["Approval.png",'Valoramos su inmueble con precisión y sistemas homologados por entidades oficiales.']
     
     ],
      conclution:['Cientos de clientes satisfechos hacen honor a nuestro código deontológico de conducta profesional, el cual hemos seguido, seguimos y seguiremos siempre cumpliendo con nuestro lema.'  ]
