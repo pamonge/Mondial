a6bb89a1cfd76665bffc066cf6c68fdb0697e029
Fiamma B <brizuelafiamma6@gmail.com>
2025-11-03 22:20:16 -0300

    style: seagro estilo para conclution y para las columnas

--- src/styles/AboutComponentStyle.js
+++ src/styles/AboutComponentStyle.js
@@ -1,9 +1,12 @@
+const conclutionColor = '#50509D';
 const AboutUsStyles = {
     container:'container mx-auto py-16 px-4',
     title: 'italic text-center py-4 font-medium text-blue-900 text-3xl ',
-    paragraph: 'text-base text-center p-5 md:max-w-5xl mx-auto text-gray-700',
-    gridItem: 'text-base  text-start p-5 md:max-w-5xl mx-auto text-gray-700   ',
-    gridCols:'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4',
+    paragraph: 'font-poppins text-base text-center p-5 md:max-w-3xl mx-auto text-gray-700',
+    gridItem: ' text-base  text-center p-2 md:max-w-5xl mx-auto text-gray-700 bg-white rounded-2xl shadow-md flex flex-col items-center',
+    gridCols:'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-11/12 mx-auto ',
     image: 'w-11/12   object-cover rounded-2xl shadow-md mx-auto my-8',
+    
+    conclution: `gap-2 font-poppins text-base text-center p-5 w-11/12 mx-auto text-white bg-[${conclutionColor}] rounded-2xl shadow-md mt-2`,
 };
 export default AboutUsStyles;
