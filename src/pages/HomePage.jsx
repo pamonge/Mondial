a0bf5c3e4db375e01ad2f9916d67e585c3b93fa0
Fiamma B <brizuelafiamma6@gmail.com>
2025-11-05 18:39:48 -0300

    feat: agrege  link componente de carousel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,13 +4,13 @@ import { MondialSlogan } from '../components/home/MondialSlogan'
 import { CardHomeComponent } from '../components/home/CardHomeComponent'
 //es la lista de servicios que se muestra en la home
 import { ListCardComponent } from '../components/services/ListCardComponent'
-
+import { CarouselComponent } from '../components/carousel/CarouselComponent'
 export const HomePage = () => {
   return (
     <div>
       <HeroComponent />
       <ListCardComponent />
-
+      <CarouselComponent />
       <MondialSlogan />
     </div>
   )
