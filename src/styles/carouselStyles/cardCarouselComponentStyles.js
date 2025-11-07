a4fc7e94556691ae65cc1a47045f4262ecdd78c6
Fiamma B <brizuelafiamma6@gmail.com>
2025-11-06 21:57:01 -0300

    style:cambio de estilos para carousel, falta ajustes y exportar estilos

--- src/styles/carouselStyles/cardCarouselComponentStyles.js
+++ src/styles/carouselStyles/cardCarouselComponentStyles.js
@@ -1,6 +1,6 @@
 const cardCarouselStyles = {
-    cardContainer: 'h-full w-[85vw] max-w-[480px] mx-auto flex-shrink-0 cursor-pointer bg-white px-4 sm:px-6 py-4 sm:py-6 rounded-2xl shadow-sm hover:shadow-2xl hover:shadow-blue-400 transition-all duration-300 border border-gray-100 hover:border-blue-800', 
-    imgContainer: 'rounded-2xl overflow-hidden mb-4 sm:mb-6 relative h-[35vh] min-h-[200px] max-h-[280px]',
+    cardContainer: 'h-full w-[98%] flex-shrink-0 cursor-pointer   rounded-2xl   ', 
+    imgContainer: 'rounded-2xl overflow-hidden mb-4 sm:mb-6 relative h-[35vh] h-[400px] md:h-[500px]',
     imgCard: "w-full h-full object-cover transition-transform duration-500 hover:scale-110",
     spanCategory: 'absolute top-3 left-4 border border-blue-300 text-xs sm:text-sm rounded-xl px-3 sm:px-4 py-2 font-bold capitalize bg-white/95 text-blue-700 shadow-sm',
     textContainer: 'px-2 sm:px-0 flex flex-col gap-3 sm:gap-4',
