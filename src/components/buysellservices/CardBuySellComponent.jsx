ed2b43b55d1c7570bcb7b283a552cf2f5053682c
IESTAE <pmonge@fundacionedison.org.ar>
2025-11-07 01:35:49 -0300

    Add: googleCarousel

--- src/components/buysellservices/CardBuySellComponent.jsx
+++ src/components/buysellservices/CardBuySellComponent.jsx
@@ -8,10 +8,6 @@ import surface from '../../assets/icons/servicesPage/surface.png'
 
 // Tarjeta individual para la lista de servicios
 export const CardBuySellComponent = ({ info, link, desc }) => {
-    console.log(info.img);
-    console.log(info.location);
-    console.log(info.sup)
-    console.log(info.rooms)
     return ( 
         <div className={servicesStyle.cardContainer}>
             <div className={servicesStyle.imgContainer}>
---
+++ src/components/googleCarousel/googleCarousel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const googleCarousel = () => {
+  return (
+    <div>
+        googleCarousel
+    </div>
+  )
+}
