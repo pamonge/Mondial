2cdfb259c8462a2fc8ef85a42a3e6427617fe20a
Fiamma B <brizuelafiamma6@gmail.com>
2025-11-08 18:35:23 -0300

    style: cambie la proporcion en la q se ven las tarjetas

--- src/styles/carouselStyles/carouselComponentStyles.js
+++ src/styles/carouselStyles/carouselComponentStyles.js
@@ -3,7 +3,7 @@ const carouselComponentStyles = {
     mainContainer: "container mx-auto py-8 px-4 ",
     overflowContainer: "overflow-hidden",
     card: "flex transition-transform duration-500 ease-in-out space-x-1",
-    data: "w-[50vw]  flex-shrink-0 py-4",
+    data: "w-[55vw]  flex-shrink-0 py-4",
     getCenter: "max-w-md mx-auto",
     leftButton: "absolute left-4 top-1/2 transform -translate-y-1/2 bg-white/90 hover:bg-white p-3 rounded-full shadow-lg z-10  transition-all hover:scale-110 hidden md:block",
     rightButton: "absolute right-4 top-1/2 transform -translate-y-1/2 bg-white/90 hover:bg-white p-3 rounded-full shadow-lg z-10 transition-all hover:scale-110 hidden md:block",
