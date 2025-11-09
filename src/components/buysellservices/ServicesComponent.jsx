5b242eb14391b2fe28285e89b588d9b667ea8070
Fiamma B <brizuelafiamma6@gmail.com>
2025-11-09 16:44:29 -0300

    style:no cambie nada acá

--- src/components/buysellservices/ServicesComponent.jsx
+++ src/components/buysellservices/ServicesComponent.jsx
@@ -4,13 +4,13 @@ import appStyles from '../../styles/appStyles'
 export const ServicesComponent = ({ title, text, satisfy, list }) => {
   return (
     <div className='flex flex-col gap-8 p-10'>
-        <h2 className={appStyles.title}>{title}</h2>
-        <p className='text-center' >{text}</p>
-        <p className='text-center satisfy-regular text-3xl' >{satisfy}</p>
+      <h2 className={appStyles.title}>{title}</h2>
+      <p className='text-center' >{text}</p>
+      <p className='text-center satisfy-regular text-3xl' >{satisfy}</p>
 
-        <div className={list? 'flex' : 'hidden'}  >
-          {list}
-        </div>
+      <div className={list ? 'flex' : 'hidden'}  >
+        {list}
+      </div>
     </div>
   )
 }
