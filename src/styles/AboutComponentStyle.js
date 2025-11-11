b621ab64ec33a3e38894d21b00be4519489b89fe
Fiamma B <brizuelafiamma6@gmail.com>
2025-11-11 18:05:05 -0300

    style: ahora en movile se ven dos columnas

--- src/styles/AboutComponentStyle.js
+++ src/styles/AboutComponentStyle.js
@@ -4,7 +4,7 @@ const AboutUsStyles = {
     title: 'italic text-center py-4 font-medium text-blue-900 text-3xl ',
     paragraph: 'font-poppins text-base text-center p-5 md:max-w-3xl mx-auto text-gray-700',
     gridItem: ' text-base  text-center p-2 md:max-w-5xl mx-auto text-gray-700 bg-white rounded-2xl shadow-md flex flex-col items-center',
-    gridCols:'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-11/12 mx-auto ',
+    gridCols:'grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 w-11/12 mx-auto ',
     image: 'w-11/12   object-cover rounded-2xl shadow-md mx-auto my-8',
     
     conclution: `gap-2 font-poppins text-base text-center p-5 w-11/12 mx-auto text-white bg-[${conclutionColor}] rounded-2xl shadow-md mt-2`,
