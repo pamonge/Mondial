a75070d40aec22a588deb5046d65f2a1024116c1
Fiamma B <brizuelafiamma6@gmail.com>
2025-11-10 23:58:59 -0300

    refactor:cambie ruta de img

--- src/components/buysellservices/CardBuySellComponent.jsx
+++ src/components/buysellservices/CardBuySellComponent.jsx
@@ -8,11 +8,11 @@ import surface from '../../assets/icons/servicesPage/surface.png'
 
 // Tarjeta individual para la lista de servicios
 export const CardBuySellComponent = ({ info, link, desc }) => {
-    return ( 
+    return (
         <div className={servicesStyle.cardContainer}>
             <div className={servicesStyle.imgContainer}>
                 <img
-                    src={`src/assets/${info.img}`}
+                    src={`${info.img}`}
                     alt={info.location}
                     className={servicesStyle.imgCard}
                 />
