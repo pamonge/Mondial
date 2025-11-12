0388db40b133bbcf0d3207096337a8549824d192
Fiamma B <brizuelafiamma6@gmail.com>
2025-11-11 22:41:58 -0300

    feat: envuelvo la tarjeta en linkComponent

--- src/components/carousel/CardCarouselComponent.jsx
+++ src/components/carousel/CardCarouselComponent.jsx
@@ -2,42 +2,37 @@ import React from 'react'
 import { LinkComponent } from '../navigation/LinkComponent'
 import cardCarouselStyles from '../../styles/carouselStyles/cardCarouselComponentStyles'
 
-// Componente que se renderiza en ./CarouselComponent.jsx
-
-export const CardCarouselComponent = ({ info, onClick }) => {
+export const CardCarouselComponent = ({ info, link }) => {
   return (
-    <div
+    <div className={`${cardCarouselStyles.cardContainer}`}>
+      <LinkComponent
+        src={link}
+        desc={
+          <div className={`${cardCarouselStyles.imgContainer} group`}>
+            <img
+              src={`${info.img}`}
+              alt={info.title}
+              className={cardCarouselStyles.imgCard}
+            />
+            <div className={cardCarouselStyles.bottomGradient}></div>
+            <div className={cardCarouselStyles.hoverOverlay}></div>
+            <div className={cardCarouselStyles.centerText}><span>Ver detalle</span></div>
 
-      className={`${cardCarouselStyles.cardContainer} `}
-      onClick={onClick}
-    >
-      < div className={`${cardCarouselStyles.imgContainer} group`}>
-        <img
-          src={`${info.img}`}
-          alt={info.title}
-          className={cardCarouselStyles.imgCard}
-        />
-        {/*degrade negro para q se vea bien el texto*/}
-        <div className={cardCarouselStyles.bottomGradient}></div>
-        {/*degrade del hover y texto del hover*/}
-        <div className={cardCarouselStyles.hoverOverlay}></div>
-        <div className={cardCarouselStyles.centerText}><span>Ver detalle</span></div>
-
-        <div className="absolute bottom-4 left-4 p-4   w-[calc(100%-2rem)] z-20">
-          <h3 className="font-semibold text-lg text-white">
-            {info.location}
-          </h3>
-          <p className="text-base text-white">
-            {info.address}
-          </p>
-          <p className="text-base text-white">
-            {info.rooms} habitaciones ~ {info.sup}
-          </p>
-        </div>
-      </div>
-
-
-
+            <div className="absolute bottom-4 left-4 p-4 w-[calc(100%-2rem)] z-20">
+              <h3 className="font-semibold text-lg text-white">
+                {info.location}
+              </h3>
+              <p className="text-base text-white">
+                {info.address}
+              </p>
+              <p className="text-base text-white">
+                {info.rooms} habitaciones ~ {info.sup}
+              </p>
+            </div>
+          </div>
+        }
+        style="block"
+      />
     </div>
   )
 }
