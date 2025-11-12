e422c78c94a8a912e16c8c7145b794255d58eb7d
Fiamma B <brizuelafiamma6@gmail.com>
2025-11-11 22:48:18 -0300

    solo agrege un comentario

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 // Renderiza los detalles de las propiedades cuando uno clickea 'Comprar' o 'Alquilar' en BuyPage o RentPage
 export const DetailPage = () => {
+
   return (
     <div>DetailPage
 
