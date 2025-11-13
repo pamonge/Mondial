b2e1563b2ddc958f94e96ac76cbf61ca75bfa0b6
Fiamma B <brizuelafiamma6@gmail.com>
2025-11-13 20:44:39 -0300

    styles: importacion de DetailComponentSyle y incorporacion de sus estilos en location y price

--- src/components/detailpage/DetailPageComponent.jsx
+++ src/components/detailpage/DetailPageComponent.jsx
@@ -3,25 +3,26 @@ import { useLocation } from 'react-router-dom'
 import { DescriptionDetailCardComponent } from './DescriptionDetailCardComponent'
 import { CaracteristicsDetailCardComponent } from './CaracteristicsDetailCardComponent'
 import locationIcon from '../../assets/icons/placeMarker.png'
-
+import detailStyle from '../../styles/NewsStyles/DetailComponentStyle'
 export const DetailPageComponent = () => {
     const location = useLocation()
-    const info = location.state 
+    const info = location.state
     return (
-        <div>
-        <h2>{info.location}</h2>
-        <div>
-            <div>
-            <img src={locationIcon} alt="place marker" />
-            <p>{info.address}</p>
+        <div className={detailStyle.container}>
+            <h2 className={detailStyle.title}>{info.location}</h2>
+            <div className={detailStyle.containerCreatorNews}>
+                <div className='flex gap-2'>
+                    <img className={detailStyle.meta} src={locationIcon} alt="place marker" />
+                    <p className={detailStyle.meta} >{info.address}</p>
+                </div>
+                <span className={detailStyle.price} >{info.price}</span>
+
+                {/* Hay que ver el carousel */}
+
             </div>
-            <span>{info.price}</span>
-            
-            {/* Hay que ver el carousel */}
-            
+
             <DescriptionDetailCardComponent data={info.description} />
-            <CaracteristicsDetailCardComponent data={info.caracteristics}  />
-        </div>
+            <CaracteristicsDetailCardComponent data={info.caracteristics} />
         </div>
     )
-    }
+}
