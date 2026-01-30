de8848a43dfff4c2270616b564d39ce198154241
Fiamma B <brizuelafiamma6@gmail.com>
2026-01-30 16:10:11 -0300

    feat:se agrego datos de id 3 aticoBeniajam

--- src/data/dataServices.js
+++ src/data/dataServices.js
@@ -10,6 +10,30 @@ import img1403144965 from '../assets/property/almeria10/1403144965.jpg';
 import img1403144966 from '../assets/property/almeria10/1403144966.jpg';
 import img1403144967 from '../assets/property/almeria10/1403144967.jpg';
 
+//aticoAvileses images
+
+//aticoBeniajam
+import img1390795609 from '../assets/property/aticoBeniajan/1390795609.jpg';
+import img1390795610 from '../assets/property/aticoBeniajan/1390795610.jpg';
+import img1390795611 from '../assets/property/aticoBeniajan/1390795611.jpg';
+import img1390795908 from '../assets/property/aticoBeniajan/1390795908.jpg';
+import img1390795909 from '../assets/property/aticoBeniajan/1390795909.jpg';
+import img1390795910 from '../assets/property/aticoBeniajan/1390795910.jpg';
+import img1390795911 from '../assets/property/aticoBeniajan/1390795911.jpg';
+import img1390795912 from '../assets/property/aticoBeniajan/1390795912.jpg';
+import img1390795966 from '../assets/property/aticoBeniajan/1390795966.jpg';
+import img1390795985 from '../assets/property/aticoBeniajan/1390795985.jpg';
+import img1390795986 from '../assets/property/aticoBeniajan/1390795986.jpg';
+import img1390795988 from '../assets/property/aticoBeniajan/1390795988.jpg';
+import img1390832169 from '../assets/property/aticoBeniajan/1390832169.jpg';
+import img1390832252 from '../assets/property/aticoBeniajan/1390832252.jpg';
+import img1390832253 from '../assets/property/aticoBeniajan/1390832253.jpg';
+import img1390832254 from '../assets/property/aticoBeniajan/1390832254.jpg';
+import img1390832331 from '../assets/property/aticoBeniajan/1390832331.jpg';
+import img1390832335 from '../assets/property/aticoBeniajan/1390832335.jpg';
+import img1390832336 from '../assets/property/aticoBeniajan/1390832336.jpg';
+
+
 import img from '../assets/img.jpg'
 //info de servicios de comprar, vender y alquilar usado en carousel y paginas de servicios
 const dataServices = {
@@ -109,30 +133,71 @@ const dataServices = {
                 ]
             },{
                 id: 3,
-                img: img,
-                price: '545.000 €',
-                location: 'Piso en Murcia centro',
-                address: 'C. San Anton',
-                sup: '55 m2',
-                rooms: '2',
-                bath: '1',
-                description: `Ofrecemos esta maravillosa vivienda de 55 m2, situada en una ubicación inmejorable, en pleno centro de Murcia, en la Gran Vía, frente al jardín de Santa Isabel.
-
-                Este encantador hogar cuenta con dos amplias habitaciones, perfectas para adaptarse a tus necesidades, salón y cocina, así como un baño completo y un aseo, también dispone de un balcón que te regala sus vistas hacia el jardín de Santa Isabel. Esta vivienda es ideal para familias que deseen entrar a vivir con comodidad. Tiene aire acondicionado y se alquila con los siguientes electrodomésticos: frigorífico, horno, vitrocerámica y lavadora.
-                                
-                No pierdas la oportunidad de vivir en una de las zonas más vibrantes de la ciudad. ¡Contáctanos para más información y para concertar una visita!`,
+                img: img1390795609,
+                imgArray: [
+                        img1390795609,
+                        img1390795610,
+                        img1390795611,
+                        img1390795908,
+                        img1390795909,
+                        img1390795910,
+                        img1390795911,
+                        img1390795912,
+                        img1390795966,
+                        img1390795985,
+                        img1390795986,
+                        img1390795988,
+                        img1390832169,
+                        img1390832252,
+                        img1390832253,
+                        img1390832254,
+                        img1390832331,
+                        img1390832335,
+                        img1390832336,
+                    ],
+                 price: '€ 159.000',
+                priceM2: "€/m² 2.481 ", 
+                resume: 'Atico Beniaján',
+                address: "Calle Algezares 112, Beniaján, Pedanías Este, Murcia",
+                
+                description:  `Espectacular ático dúplex en pleno centro de Beniaján, en una de las mejores ubicaciones de la localidad. Edificio del año 2006 en perfecto estado de conservación. Superficie construida: 110m2.
+    
+    Vivienda alquilada con un alquiler mensual de 650 euros, finalización del contrato de alquiler 2 años. Vivienda lista para entrar a vivir. Situación inmejorable: a 2 minutos andando de todos los servicios (supermercados, farmacia, colegios, centro de salud, bares y restaurantes). Perfectamente comunicada con Murcia capital en menos de 10 minutos por autovía.`,
+    
+                general: [
+                    "2 habitaciones amplias con armarios empotrados",
+                    "1 baño completo",
+                    "Salón-comedor amplio con salida a terraza y balcón",
+                    "Cocina independiente equipada",
+                    "2 trasteros incluidos (ideal para almacenamiento extra)",
+                    "Calefacción y aire acondicionado"
+                ],
+            
                 caracteristics: [
-                    'Aire acondicionado',
-                    'Ascensor',
-                    'Balcon',
-                    'Conserje',
-                    'Terraza',
-                    'Armarios empotrados',
-                    'Calefacción',
-                    'Planta (2)',
-                    'Reformado',
-                    'Ano de construcción (1957)'
-                ]
+        "Planta baja del ático: Amplio salón comedor muy luminoso, cocina independiente totalmente equipada, 1 dormitorio doble y 1 baño completo",
+        "Planta superior: 2 dormitorios dobles (el principal con mucha luz) y segundo baño completo",
+        "Totalmente exterior a 4 vientos, mucha luz natural todo el día",
+        "Terraza grande en la planta superior ideal para disfrutar las vistas y las tardes de verano",
+        "Plaza de garaje y trastero incluidos en el precio",
+        "Aire acondicionado por conductos (frío/calor)",
+        "Carpintería exterior de aluminio con climalit, persianas eléctricas, suelos de mármol, puertas de madera maciza"
+    ],
+            
+                energyCertification: [
+                    'consumo: 0.1 kWh/m² año',
+                    'emisiones: 0.1m C02/m² año',
+                ],
+            
+                building: [
+                    'Planta: 3, Exterior',
+                    'Con ascensor'
+                ],
+            
+                equipment: [
+                    'calefaccion: Individual',
+                    'combustible calefacción: Bomba de frío/calor',
+                    'Aire Acondicionado'
+                ],
             }
         ]
     },
