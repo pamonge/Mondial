ee3f837d1f7d2c03415c563d9ca9aaab232d975a
Pablo Monge <pablomonge55@gmail.com>
2026-01-30 13:27:35 -0300

    Fix: info, detailCard

--- src/components/detailpage/DetailPageComponent.jsx
+++ src/components/detailpage/DetailPageComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 import { DescriptionDetailCardComponent } from './DescriptionDetailCardComponent'
-import { CaracteristicsDetailCardComponent } from './CaracteristicsDetailCardComponent'
+import { DetailCardComponent } from './DetailCardComponent'
 import locationIcon from '../../assets/icons/placeMarker.png'
 import detailStyle from '../../styles/NewsStyles/DetailComponentStyle'
 import { CarouselComponent } from '../../components/carouselDetail/CarouselComponent'
@@ -12,21 +12,24 @@ export const DetailPageComponent = ({ data }) => {
     // console.log(`DetailPageComp tiene: ${info.description}`)
     return (
         <div className={detailStyle.container}>
-            <h2 className={detailStyle.title}>{info.location}</h2>
+            <h2 className={detailStyle.title}>{info.resume}</h2>
             <div className={detailStyle.containerCreatorNews}>
                 <div className='flex gap-2'>
                     <img className={detailStyle.meta} src={locationIcon} alt="place marker" />
                     <p className={detailStyle.meta} >{info.address}</p>
                 </div>
-                <span className={detailStyle.price} >{info.price}</span>
-
-                {/* Hay que ver el carousel */}
+                <div className='flex flex-col'>
+                    <span className={detailStyle.price} >{info.price}</span>
+                    <span className={detailStyle.price} >{info.priceM2}</span>
+                </div>
 
             </div>
-            {/* Carousel provisorio */}
             <CarouselComponent />
             <DescriptionDetailCardComponent data={info} />
-            <CaracteristicsDetailCardComponent data={info.caracteristics} />
+            <DetailCardComponent title='Características' data={info.caracteristics} />
+            <DetailCardComponent title='Certificación Energética' data={info.energyCertification} />
+            <DetailCardComponent title='Edificio' data={info.building} />
+            <DetailCardComponent title='Equipamiento' data={info.equipment} />
             
             {/* Faltan propiedades Similares */}
     
--- src/data/property/almeria10.js
+++ src/data/property/almeria10.js
@@ -12,69 +12,6 @@ import img1403144967 from '../../assets/property/almeria10/1403144967.jpg';
 
 const almeria10 = {
     id:1,
-    descripcion: `¡OPORTUNIDAD ÚNICA! Piso en venta en Murcia capital con excelente ubicación. Magnífico piso en venta en el corazón de Murcia, en una zona privilegiada, tranquila y bien comunicada, cerca de todos los servicios: comercios, colegios, transporte público y a pocos minutos del centro y la playa. La vivienda es totalmente exterior, muy luminosa y ventilada, con orientación ideal que garantiza luz natural durante todo el día.
-    
-    El inmueble se encuentra en perfecto estado de conservación, listo para entrar a vivir. Edificio con vecinos tranquilos y zona comunitaria bien mantenida. Perfecto tanto para vivienda habitual como para inversión.
-    
-    ¡No dejes escapar esta oportunidad!`,
-
-    caracteristicas: [
-        "2 habitaciones amplias con armarios empotrados",
-        "1 baño completo",
-        "Salón-comedor amplio con salida a terraza y balcón",
-        "Cocina independiente equipada",
-        "2 trasteros incluidos (ideal para almacenamiento extra)",
-        "Calefacción y aire acondicionado"
-    ],
-
-    precio: {
-        valor: 129000,
-        moneda: "€",
-        precioPorM2: 2481,
-        unidadPrecioM2: "eur/m2",
-    },
-
-    caracteristicasBasicas: {
-        metrosCuadradosConstruidos: 52,
-        dormitorios: 2,
-        banos: 1,
-        terrazas: 1,
-        estadoVivienda: "Segunda mano/Buen estado",
-        equipamiento: ["Armarios empotrados", "Trastero"]
-    },
-
-    certificacionEnergetica: {
-        consumo: {
-        valor: 0.1,
-        unidad: "kWh/m2 año"
-        },
-        emisiones: {
-        valor: 0.1,
-        unidad: "C02/m2 año"
-        }
-    },
-
-    edificio: {
-        planta: "1",
-        orientacion: "Exterior",
-        ascensor: true
-    },
-
-    equipamiento: {
-        calefaccion: "Individual",
-        combustible: "Gas natural",
-        aireAcondicionado: true
-    },
-
-    ubicacion: {
-        direccion: "Avenida Ciudad de Almería 10, El Carmen, Sur, Murcia",
-        planta: "1",
-        barrio: "El Carmen",
-        distrito: "Sur",
-        ciudad: "Murcia",
-        provincia: "Murcia",
-    },
-
     imagenes: [
         img1403144917,
         img1403144918,
@@ -87,6 +24,59 @@ const almeria10 = {
         img1403144965,
         img1403144966,
         img1403144967,
+    ],
+    
+    price: '€ 129.000',
+    priceM2: "2.481 €/m2", 
+    resume: 'Piso en Murcia centro',
+    address: {
+        direction: "Avenida Ciudad de Almería 10, El Carmen, Sur, Murcia",
+        planta: "1",
+        barrio: "El Carmen",
+        distrito: "Sur",
+        ciudad: "Murcia",
+        provincia: "Murcia",
+    }, 
+    
+    descripcion: `¡OPORTUNIDAD ÚNICA! Piso en venta en Murcia capital con excelente ubicación. Magnífico piso en venta en el corazón de Murcia, en una zona privilegiada, tranquila y bien comunicada, cerca de todos los servicios: comercios, colegios, transporte público y a pocos minutos del centro y la playa. La vivienda es totalmente exterior, muy luminosa y ventilada, con orientación ideal que garantiza luz natural durante todo el día.
+    
+    El inmueble se encuentra en perfecto estado de conservación, listo para entrar a vivir. Edificio con vecinos tranquilos y zona comunitaria bien mantenida. Perfecto tanto para vivienda habitual como para inversión.
+    
+    ¡No dejes escapar esta oportunidad!`,
+
+    general: [
+        "2 habitaciones amplias con armarios empotrados",
+        "1 baño completo",
+        "Salón-comedor amplio con salida a terraza y balcón",
+        "Cocina independiente equipada",
+        "2 trasteros incluidos (ideal para almacenamiento extra)",
+        "Calefacción y aire acondicionado"
+    ],
+
+    caracteristics: [
+        'superficie: 52 m2',
+        'dormitorios: 2',
+        'baños: 1',
+        'terraza: 1',
+        'condicion: Segunda mano/Buen estado',
+        'Armarios empotrados',
+        'Trastero',
+    ],
+
+    energyCertification: [
+        'consumo: 0.1 kWh/m2 año',
+        'emisiones: 0.1m C02/m2 año',
+    ],
+
+    building: [
+        'Planta: 1, Exterior',
+        'Con ascensor'
+    ],
+
+    equipment: [
+        'calefaccion: Individual',
+        'combustible calefacción: Gas natural',
+        'Aire Acondicionado'
     ],
 }
 
